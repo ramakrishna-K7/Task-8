0f4625cafbd12defbe3c88c62c1e6a843e383fc0
ramakrishna-K7 <ramakrishnakannekanti868@gmail.com>
2025-06-06 10:57:22 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
- HelloWorld.java public class HelloWorld
-  { public static void main(String[] args) {
-     System.out.println("Hello, Jenkins +Maven!"); 
-   } 
- }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
